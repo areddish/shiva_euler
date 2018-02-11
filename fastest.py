b21d65cf51b6fef0bac3692aeed6a1b2330ea3d6
Andrew Reddish <areddish@foosion.net>
2018-02-11 18:12:10 -0500

    add fastest

--- fastest.py
+++ fastest.py
@@ -4,8 +4,9 @@ def pythagorean_triplet():
     :return: [int] Product of the Pythagorean Triplet.
     """
     for c in range(3, 1000):
+        c_sq = c**2
         for b in range(2, c-1):
-            for a in range(1, b-1):
-                if (a + b + c) == 1000 and ((a**2) + (b**2)) == (c**2):
-                    return (a * b * c)
+            a = 1000 - b -c
+            if ((a**2) + (b**2)) == c_sq:
+                return (a * b * c)
     return None
--- testbed.py
+++ testbed.py
@@ -1,8 +1,10 @@
 import shiva
 import faster
+import fastest
 from harness import average_run
    
 if __name__ == "__main__":
    print("Running tests:")
    print("Shiva average: ", average_run(shiva.pythagorean_triplet)*1000, "ms")
    print("faster average: ", average_run(faster.pythagorean_triplet)*1000, "ms")
+   print("fastest average: ", average_run(fastest.pythagorean_triplet)*1000, "ms")
